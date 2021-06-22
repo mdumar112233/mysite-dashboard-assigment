35b58570b291dc2aa7bcf1e1280122a3cda427f2
md umar faruk <mdumar11233>
2021-06-22 16:20:07 +0600

    added new code

--- src/Components/Share/Sidebar/Sidebar.js
+++ src/Components/Share/Sidebar/Sidebar.js
@@ -7,14 +7,14 @@ import './Sidebar.css';
 const Sidebar = () => {
     return (
           <div className="col-md-2 sidebar-container">
-               <div className="project">
+               <div className="project mt-4" role="button">
                     <img  src={project} alt="project" /> <span className='text-secondary mt-3'>Projects</span>
                 </div>
-                <div className="profile">
+                <div className="profile mt-4" role="button">
                     <img src={profile} alt="profile" /> <span className='text-secondary'>Organisation Profile</span>
                 </div>
-                <div className="control">
-                    <img src={control} alt="control" /> <span className='text-secondary'>Access Control</span>
+                <div className="control mt-3" role="button">
+                    <span className='icon'></span><img src={control} alt="control" /> <span className='control-btn' style={{color: '#673AB7'}}>Access Control</span>
                 </div>
              </div>
         );
